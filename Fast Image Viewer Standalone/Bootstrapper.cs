9529f6fdfbb4f170aafd12ab2f0fdd382162c731
Nevaran <nevaranreal@gmail.com>
2019-06-13 23:18:57 +0300

    Revert "failed attempt to remove mvvm"
    This reverts commit f19e50d239069f1f9faecec8b41afb2f37b21011.

---
+++ Fast Image Viewer Standalone/Bootstrapper.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using Caliburn.Micro;
+using FIVStandard.ViewModels;
+
+namespace FIVStandard
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper() 
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<MainViewModel>();
+        }
+    }
+}
