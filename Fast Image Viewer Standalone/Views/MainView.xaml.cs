19bb8d9ef1089005f6efb7ad9ae8cc0501ec494e
Nevaran <nevaranreal@gmail.com>
2019-06-14 15:17:10 +0300

    Revert "The Last Caliburn(Aribender)"
    This reverts commit 962533ec5e15726870b09a83a442215547cbb503.

--- Fast Image Viewer Standalone/Views/MainView.xaml.cs
+++ Fast Image Viewer Standalone/Views/MainView.xaml.cs
@@ -4,12 +4,11 @@ namespace FIVStandard.Views
 {
     public partial class MainView : MetroWindow
     {
-        public static MainView mainView;//TODO: temp
+        public static MainView mainView;
 
         public MainView()
         {
             mainView = this;
-
             //InitializeComponent();
         }
     }
